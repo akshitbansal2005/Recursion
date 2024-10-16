432fcae463a37feb0c3e6b77c5f36dfe3c7c22b6
Akshit Bansal <155195875+akshitbansal2005@users.noreply.github.com>
2024-10-16 12:19:20 +0530

    Create Reverse_Number_recursion.cpp

---
+++ Reverse_Number_recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void rev(int i) {
+    if (i > 0) {
+        cout << (i % 10); 
+        rev(i / 10);      
+    }
+}
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Reversed number is: ";
+    rev(num);
+    cout << endl;
+}
+//Output
+/*Enter a number: 35
+Reversed number is: 53
+*/
